1455142cd5e959b2137046d501f310e9fbcb34bf
suleyman <suleyman.sevimli1@gmail.com>
2020-05-20 23:24:22 +0300

    user model create & register

---
+++ models/User.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+
+const UserSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        minlength: 5,
+        required: true
+    },
+
+});
+
+module.exports = mongoose.model('user', UserSchema);
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,29 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const User = require('../models/User')
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.post('/register', function (req, res, next) {
+  const { username, password } = req.body;
+
+  const user = new User({
+    username,
+    password
+  })
+
+  user.save().then(data => {
+    res.status(201).json({
+      ...data
+    })
+  }).catch(err => {
+    res.json(err)
+  })
+
+});
+
 
 module.exports = router;
